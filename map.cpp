733715939d4c64d749f243adaf28a6e372ae338f
yook-jongho <39684697+yook-jongho@users.noreply.github.com>
2021-05-29 02:12:01 +0900

    Add files via upload

--- map.cpp
+++ map.cpp
@@ -1,36 +1,38 @@
 #include <fstream>
 #include <random>
-#include <iostream>
-#include <ncurses.h>
-#include <vector>
-#include <string>
 #include <cstring>
+#include <ncurses.h>
+
 #include "map.h"
 #include "snake.h"
 #include "Controll.h"
+#include "manager.h"
 
 using namespace std;
 Snake s;
 Controll c;
+Manager manager;
 int i = 0;
+bool check = true;
 
-void Map::render()
+//게임 화면을 그리는 함수
+bool Map::render()
 {
 
     initscr();
     resize_term(60, 100);
 
     border('+', '+', '+', '+', '*', '*', '*', '*');
-    refresh();
+    refresh(); //환기하는거
 
-    mvprintw(2, 2, "<snake game>");
+    mvprintw(2, 9, "<<snake game>>");
 
-    WINDOW *gamebox;
+    WINDOW *gamebox; //게임 화면을 나타내주는 박스 = 뱀이 기어다니는 곳
     gamebox = newwin(25, 25, 3, 3);
 
     WINDOW *scoreBox;
     scoreBox = newwin(12, 10, 3, 30);
-    box(scoreBox, 0, 0);
+    box(scoreBox, 0, 0); //테두리를 표시. box 영역
 
     upDate();
     for (int i = 0; i < 25; i++)
@@ -40,12 +42,27 @@ void Map::render()
             wprintw(gamebox, "%c", v[i][j]);
         }
     }
+    if (!check)
+    {
+        mvprintw(2, 9, "<<game over>>");
+    }
+    refresh();
     wrefresh(gamebox);
     wrefresh(scoreBox);
 
     //inner box
     //s.Move(14);
     delwin(gamebox);
+    delwin(scoreBox);
+
+    if (!check)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
 //map 파일을 받아와 배열에 저장.
@@ -81,45 +98,65 @@ void Map::getMap()
 
 void Map::upDate()
 {
+    //뱀 머리의 방향 확인.
     char a = s.direction;
     char b = c.controll(a);
-    bool check = c.check_dir(a, b);
+    check = c.check_dir(a, b);
 
-    for (int i = 0; i < 25; i++)
+    if (!check)
     {
-        for (int j = 0; j < 25; j++)
+        for (int i = 0; i < 25; i++)
         {
-            if (v[i][j] == '0')
-                v[i][j] = ' ';
-            if (v[i][j] == '@')
+            for (int j = 0; j < 25; j++)
             {
-                s.head_x = j;
-                s.head_y = i;
-                v[i][j] = ' ';
+                if (v[i][j] == '0' || v[i][j] == '@')
+                    v[i][j] = ' ';
             }
         }
     }
-    //s.makeBon(s.head_x, s.head_y, a, check);
-    s.Move(a);
-    s.Draw(v, s.head_x, s.head_y, 3, a);
-    s.direction = b;
-    item(i);
-    i++;
+    else
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            for (int j = 0; j < 25; j++)
+            {
+                if (v[i][j] == '0')
+                    v[i][j] = ' ';
+                if (v[i][j] == '@')
+                {
+                    s.head_x = j;
+                    s.head_y = i;
+                    v[i][j] = '0';
+                }
+            }
+        }
+
+        s.Move(a);
+        s.Draw(v, s.head_x, s.head_y, 3, a);
+        s.direction = b;
+        item();
+        i++;
+    }
 }
 
-void Map::item(int count)
+void Map::item()
 {
-    if (count % 10 == 0)
+    int a, b;
+    if (i == 50)
     {
+        if (a >= 0 || b >= 0)
+        {
+            v[a][b] = ' ';
+        }
         random_device rd;
         mt19937 gen(rd());
 
-        uniform_int_distribution<int> dis(1, 24);
+        uniform_int_distribution<int> dis(5, 28);
 
-        int a = dis(gen);
-        int b = dis(gen);
+        a = dis(gen);
+        b = dis(gen);
 
         v[a][b] = '$';
-        count = 0;
+        i = 0;
     }
 }
--- map.h
+++ map.h
@@ -1,10 +1,10 @@
 class Map
 {
 public:
-    char v[25][25] = {{'0'}};
+    char v[25][25] = {{'0'}}; //멥 배열
 
-    void getMap();
-    void render();
+    void getMap(); //map1.txt 받아옴
+    bool render();
     void upDate();
-    void item(int count);
+    void item(); //item 멥에 표시해주는 함수
 };
